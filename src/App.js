0eca7f8e0ca3b684b96bb02fc5c0ea8716ed6c23
Jose <magoclownxinigami@gmail.com>
2021-07-08 12:28:57 -0500

    Update menu-items to navigate wirthRouter HOC

--- src/App.js
+++ src/App.js
@@ -1,13 +1,25 @@
 import React from 'react';
-import "./App.css";
+import { Switch, Route } from 'react-router-dom';
+
+import './App.css';
 
 import HomePage from './pages/homepage/homepage.componen';
 
+const HatsPage = () => (
+  <div>
+    <h1>HATS PAGE</h1>
+  </div>
+);
+
 function App() {
   return (
-  <div>
-    <HomePage/>
-  </div>);
+    <div>
+      <Switch>
+        <Route exact path='/' component={HomePage} />
+        <Route path='/hats' component={HatsPage} />
+      </Switch>
+    </div>
+  );
 }
 
 export default App;
