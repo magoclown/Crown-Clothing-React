87c7fc6171516afd92b6f8236dfa847a233ee170
Jose <magoclownxinigami@gmail.com>
2021-07-12 16:55:29 -0500

    added cartIcon and cartDropdown components to our application, also implemented cart reducer

--- src/components/custom-button/custom-button.component.jsx
+++ src/components/custom-button/custom-button.component.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import './custom-button.style.scss';
+import './custom-button.styles.scss';
 
 const CustomButton = ({ children, isGoogleSignIn, ...otherProps }) => (
   <button
--- src/components/sign-in/sign-in.component.jsx
+++ src/components/sign-in/sign-in.component.jsx
@@ -5,7 +5,7 @@ import CustomButton from '../custom-button/custom-button.component';
 
 import { auth, signInWithGoogle } from '../../firebase/firebase.utils';
 
-import './sign-in.style.scss';
+import './sign-in.styles.scss';
 
 class SignIn extends React.Component {
   constructor(props) {
