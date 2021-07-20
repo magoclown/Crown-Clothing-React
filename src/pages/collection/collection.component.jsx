c4c42e980049f4dba858c1d077a60f6cfa80d7d6
Jose <magoclownxinigami@gmail.com>
2021-07-20 17:03:38 -0500

    updated our application App component, sign-in component and sign-up component to use hooks

--- src/pages/collection/collection.component.jsx
+++ src/pages/collection/collection.component.jsx
@@ -1,19 +1,32 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import { connect } from 'react-redux';
 
 import CollectionItem from '../../components/collection-item/collection-item.component';
 
 import { selectCollection } from '../../redux/shop/shop.selectors';
 
+import { firestore } from '../../firebase/';
+
 import './collection.styles.scss';
 
 const CollectionPage = ({ collection }) => {
+  useEffect(() => {
+    console.log('I am subscribing');
+    const unsubscribeFromCollection = firestore
+      .colleciton('collections')
+      .onSnapshot(async (snapshot) => console.log(snapshot));
+    return () => {
+      console.log('I am unsubscribing');
+      unsubscribeFromCollection();
+    };
+  }, []);
+
   const { title, items } = collection;
   return (
     <div className='collection-page'>
       <h2 className='title'>{title}</h2>
       <div className='items'>
-        {items.map(item => (
+        {items.map((item) => (
           <CollectionItem key={item.id} item={item} />
         ))}
       </div>
@@ -22,7 +35,7 @@ const CollectionPage = ({ collection }) => {
 };
 
 const mapStateToProps = (state, ownProps) => ({
-  collection: selectCollection(ownProps.match.params.collectionId)(state)
+  collection: selectCollection(ownProps.match.params.collectionId)(state),
 });
 
 export default connect(mapStateToProps)(CollectionPage);
